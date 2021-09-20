af6b41dfb7a126e519d97ed6c99eb233c06973fc
ro <ro.michi1129@gmail.com>
2021-09-20 12:53:46 +0900

    46-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("app");
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const countUpOnClick = () => {
     setNum(num + 1);
@@ -11,6 +12,17 @@ const App = () => {
   const switchShowFlagOnClick = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
