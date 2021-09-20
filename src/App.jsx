eb8e5d7e15c332c2bbbdac457bbf9df9594e4a0c
ro <ro.michi1129@gmail.com>
2021-09-20 12:36:21 +0900

    45

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const buttonOnClick = () => {
-    alert("aa");
+  const countUpOnClick = () => {
+    setNum(num + 1);
   };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>Hello world!</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
-      <button onClick={buttonOnClick}>ボタン</button>
+      <button onClick={countUpOnClick}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
